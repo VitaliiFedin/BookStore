8f1fe5c85cb253e6220746eab3d6dfd753656c14
VitaliiFedin <vitalii.fedinn@gmail.com>
2023-01-30 12:32:16 +0200

    pages, test

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
     'accounts',
+    'pages',
 
 ]
 
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
