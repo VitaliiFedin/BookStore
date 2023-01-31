2c1960657eacc35516b8d2e4f016a3051f730a12
VitaliiFedin <vitalii.fedinn@gmail.com>
2023-01-31 09:42:05 +0200

    signup, tests

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CustomCreationForm
+
 
 # Create your views here.
+class SignupPageView(CreateView):
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+    form_class = CustomCreationForm
