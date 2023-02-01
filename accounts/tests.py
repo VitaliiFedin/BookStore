1f53e9cdb13c2b97fa32da5b8a2e023550749b35
VitaliiFedin <vitalii.fedinn@gmail.com>
2023-02-01 15:10:07 +0200

    environs

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse, resolve
-from .forms import CustomCreationForm
+from django.urls import reverse
 
 
 class CustomUserTests(TestCase):
